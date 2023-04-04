e67d33b8a730c8a1ad83b6f00622516485cb43a3
Jorgeablade <jorgeablade@gmail.com>
2023-04-04 16:14:24 +0200

    ♻️ I'm dumb, same venv♻️

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/picolo/Desktop/CRUD_FastAPI_React/.venv"
+set -gx VIRTUAL_ENV "/home/picolo/Desktop/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
