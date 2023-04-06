4ed641e01c23a1bedfd580a1604487234b1c7f72
Jorgeablade <jorgeablade@gmail.com>
2023-04-06 18:23:43 +0200

    🎨 Add a login system 🎨But need to clean the code

--- app.py
+++ app.py
@@ -1,59 +1,117 @@
-from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise # Allow us to register the models
-from models import(task_pydantic, task_pydanticIn, task) # Import the models
+from fastapi import FastAPI, Depends, Request, Form, status, HTTPException
 
-# Jinja2 templates
-from fastapi import FastAPI, Request
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
+ 
+from sqlalchemy.orm import Session
+
+# Trying ot make a login system
+from fastapi_login import LoginManager
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseSettings, BaseModel, UUID4
+from fastapi_login.exceptions import InvalidCredentialsException
 
-# Create the app
+import models
+from database import SessionLocal, engine
+ 
+models.Base.metadata.create_all(bind=engine)
+
+
+class Settings(BaseSettings):
+    secret: str  # automatically taken from environment variable
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class User(UserCreate):
+    id: UUID4
+
+DEFAULT_SETTINGS = Settings(_env_file=".env")
+DB = {
+    "users": {}
+}
+
+TOKEN_URL = "/auth/token"
+ 
+templates = Jinja2Templates(directory="templates")
+ 
 app = FastAPI()
 
-# Hello world
-@app.get('/')
-async def hello_world():
-    return {"message": "Hello World"}
+manager = LoginManager(DEFAULT_SETTINGS.secret, TOKEN_URL)
+ 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.post('/task')
-# We create the function with sypplier_pydanticIn, because we are exliuding the id
-async def add_task(task_info: task_pydanticIn):
-    task_obj = await task.create(**task_info.dict(exclude_unset=True))
-    # But in the response we want to know the id, so we use task_pydantic
-    response = await task_pydantic.from_tortoise_orm(task_obj)
-    return {"status": "success", "data": response}
+@manager.user_loader()
+def load_user(username: str, db: Session = Depends(get_db)):
+    user = db.query(models.user).filter(models.user.username == username).first()
+    return user
+ 
+# Add user to database, just to test
+@app.post("/auth/register")
+def register(data: UserCreate, db: Session = Depends(get_db)):
+    new_user = models.user(username=data.username, password=data.password)
+    db.add(new_user)
+    db.commit()
+    return {"message": "User created successfully"}
 
-# Get all tasks
-@app.get('/task')
-async def get_all_tasks():
-    response = await task_pydantic.from_queryset(task.all())
-    return {"status": "success", "data": response}
+# Now we have to define a way to let the user login in our app. Therefore we will create a new route:
+@app.post("/auth/token")
+def login(data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user_input = data.username
+    password = data.password
 
-# Get a single task
-@app.get('/task/{task_id}')
-async def get_task(task_id: int, request: Request):
-    response = await task_pydantic.from_queryset_single(task.get(id=task_id))
-    return {"status": "success", "data": response}
+    user_in_db = load_user(data.username, db)
+    if not user_in_db:
+        raise HTTPException(status_code=400, detail="Incorrect (username) or password1")
+        
+    elif password != user_in_db.password:
+        raise HTTPException(status_code=400, detail="Incorrect username or (password)")
 
-# Update a task
-@app.put('/task/{task_id}')
-async def update_task(task_id: int, update_info: task_pydanticIn):
-    await task.filter(id=task_id).update(**update_info.dict(exclude_unset=True))
-    response = await task_pydantic.from_queryset_single(task.get(id=task_id))
-    return {"status": "success", "data": response}
+    access_token = manager.create_access_token(
+        data=dict(sub=user_input)
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-# Delete a task
-@app.delete('/task/{task_id}')
-async def delete_task(task_id: int):
-    await task.filter(id=task_id).delete()
-    return {"status": "success", "message": "task deleted"}
+@app.get("/")
+def home(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todo).all()
+    return templates.TemplateResponse("index.html",
+                                      {"request": request, "todo_list": todos})
+ 
+@app.post("/add")
+def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    new_todo = models.Todo(title=title)
+    db.add(new_todo)
+    db.commit()
+ 
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+ 
+ 
+@app.get("/update/{todo_id}")
+def update(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    todo.complete = not todo.complete
+    db.commit()
+ 
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+ 
+ 
+@app.get("/delete/{todo_id}")
+def delete(request: Request, todo_id: int, db: Session = Depends(get_db), user=Depends(manager)):
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    db.delete(todo)
+    db.commit()
 
-# Register the models with tortoise
-register_tortoise(
-    app,
-    db_url="sqlite://database.sqlite3", # The database url
-    modules={"models": ["models"]}, # The models.py file
-    generate_schemas=True, 
-    add_exception_handlers=True,
-)
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
--- models.py
+++ models.py
@@ -1,19 +1,20 @@
-from tortoise.models import Model
-from tortoise import fields
-from tortoise.contrib.pydantic import pydantic_model_creator
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.sql.sqltypes import DateTime
+import datetime
+from database import Base
+ 
+class Todo(Base):
+    __tablename__ = "todos"
+ 
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    complete = Column(Boolean, default=False)
+    # auto add date, but just, year, month, day
+    date = Column(DateTime, default=datetime.datetime.now().date())
 
-class task(Model):
-    id = fields.IntField(pk=True)
-    task_title  = fields.CharField(max_length=100, nullable=False)
-    task_description = fields.CharField(max_length=200, nullable=False)
-    importance = fields.IntField(default=0, max_chars=10)
-    date_of_creation = fields.DatetimeField(auto_now_add=True)
-
-# create pydantic models
-
-# create a pydantic model from the task model with all the fields
-task_pydantic = pydantic_model_creator(task, name="task")
-
-# create a pydantic model from the task model with all the fields except the id, this is used for creating a new task
-# thats why we exclude the readonly fields, so that the id is not included
-task_pydanticIn = pydantic_model_creator(task, name="taskIn", exclude_readonly=True)
+class user(Base):
+    __tablename__ = "users"
+ 
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    password = Column(String)
